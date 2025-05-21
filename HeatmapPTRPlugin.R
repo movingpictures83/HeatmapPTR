b8687a44bac2be4a435e4b46300e9911d7666918
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2025-05-21 09:40:23 -0400

    CSV

--- HeatmapPTRPlugin.R
+++ HeatmapPTRPlugin.R
@@ -57,6 +57,7 @@ p.value <- t(p.value)
 #plot.data$stars <- cut(plot.data$p.value, breaks=c(-Inf, 0.001, 0.01, 0.05, Inf), label=c("***", "**", "*", ""))  # Create column of significance labels
 
 png(height=1200, width=1200, pointsize=25, file=out_plot)
+write.csv(plot.data, paste(outputfile, "csv", sep="."))
 corrplot(plot.data, is.corr=FALSE, p.mat= p.value, sig.level = .05, tl.col = "black", cl.lim=c(-1,1), col=colorRampPalette(c("red","white", "blue"))(200), cl.ratio=0.1, cl.length = 5)
 dev.off()
 }
